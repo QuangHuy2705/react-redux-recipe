52d6db456148629bc461e2f405679ed605686647
Huy Thac Quang Nguyen <huytqnguyen@kms.com.vn>
2019-06-10 16:28:20 +0700

    add reducer

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import { mergeMap, filter, mapTo } from 'rxjs/operators'
 import { epic1, epic2 } from './epics/index'
 import { compose as recompose, defaultProps } from 'recompose'
 import StoreContainer from './store_provider'
-import { noop, functions } from 'lodash'
+import { noop, functions, isObject } from 'lodash'
 
 const reduceReducers = (reducers) => (state, action) =>
     reducers.reduce((result, reducer) => (
@@ -24,6 +24,14 @@ const normalizeEpic = (epic = noop, namespace = 'main') => {
   return epic
 }
 
+const setFnName = (fn, name) =>
+  Object.defineProperty(fn, 'name', { value: name })
+
+const normalizeReducer = (key, reducer) => {
+    const temp = reducer || noop
+    return setFnName(temp, key)
+  }
+
 class StoreBuilder {
     constructor() {
         this.store = null
@@ -39,14 +47,43 @@ class StoreBuilder {
         this.reducerMap = {}
     }
 
-    registerReducers = reducerMap => {
-        Object.entries(reducerMap).forEach(([name, reducer]) => {
-            if (!this.reducerMap[name]) this.reducerMap[name] = [];
-
-            this.reducerMap[name].push(reducer);
-        });
-        // this.store.replaceReducer(this.createRootReducer());
+    getReducers = () => {
+        return combineReducers(this.reducerMap)
     }
+ 
+    registerReducers = reducerMap => {
+        // Object.entries(reducerMap).forEach(([name, reducer]) => {
+        //     if (!this.reducerMap[name]) this.reducerMap[name] = [];
+
+        //     this.reducerMap[name].push(reducer);
+        // });
+        // this.store.replaceReducer(this.createRootReducer());
+        if (isObject(reducerMap)) {
+            let appendedReducer = Map()
+            functions(reducerMap).forEach(reducerKey => {
+              if (this.reducerRegistry.has(reducerKey)) {
+                return this
+              } else {
+                const reducer = normalizeReducer(
+                  reducerKey,
+                  reducerMap[`${reducerKey}`],
+                )
+                appendedReducer = appendedReducer.set(reducerKey, reducer)
+              }
+            })
+            this.reducerRegistry = this.reducerRegistry.merge(appendedReducer)
+            this.updateReducers()
+            return this
+          }
+    }
+
+    updateReducers() {
+        const newReducers = this.getCombineReducers()
+        this.store &&
+          this.store.replaceReducer &&
+          this.store.replaceReducer(newReducers)
+      }
+
     registerEpics = epic => {
         if (this.epicRegistry.indexOf(epic) === -1) {
             // this.epicRegistry.push(epic);
@@ -71,7 +108,7 @@ class StoreBuilder {
 
     createStore = () => {
         const epicMiddleware = createEpicMiddleware()
-        this.store = createStore(this.createRootReducer(), composeEnhancers(applyMiddleware(epicMiddleware)));
+        this.store = createStore(this.getReducers(), composeEnhancers(applyMiddleware(epicMiddleware)));
         epicMiddleware.run(this.rootEpic)
         return this
     }
